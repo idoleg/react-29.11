919692cdea53e1662933a80d24a07f061d63eb84
Oleg Shikov <ol.shikov@yandex.ru>
2019-11-29 22:07:16 +0300

    Add lesson 1

---
+++ Oleg_Shikov/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const messages = [
+    {id: 0, name: "Oleg", content: "Привет!"},
+    {id: 1, name: "Ivan", content: "как дела?"},
+    {id: 3, name: "Oleg", content: "Good"},
+    {id: 4, name: "", content: "Good"},
+]
+
+const Message = (props) => <div><b>{props.name}:</b> {props.content}</div>;
+const MessageList = function(props) {
+    return props.messages.map(
+        item => <Message name={item.name} content={item.content} key={item.id}/>);
+}
+
+ReactDOM.render(<MessageList messages={messages}/>, document.getElementById("root"))
---
+++ Oleg_Shikov/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, "src", "index.js"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    }, 
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/env", "@babel/react"]
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html"),
+            filename: "index.html"
+        })
+    ]
+}
