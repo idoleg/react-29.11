51d1033edd53da44faf9a0e21008b34be4f17e05
VBaykin <vbaykin@mail.ru>
2019-12-09 05:17:19 +0400

    первый вариант

--- chipcom/webpack.config.js
+++ chipcom/webpack.config.js
@@ -3,24 +3,48 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: path.resolve(__dirname, "src", "index.js"),
+	entry: path.resolve(__dirname, "src", "index.jsx"),
 	output: {
 		path: path.resolve(__dirname, "dist"),
-		filename: 'bundle.js',
+		filename: 'bundle.jsx',
 	},
-
 	module: {
 		rules: [
 			{
 				test : /\.(js|jsx)$/ ,
-				include : path. resolve (__dirname , "src" ) ,
+				include : path. resolve (__dirname, "src" ) ,
 				loader : 'babel-loader' ,
 				exclude : /node_modules/ ,
-				options : {
-					presets : [ '@babel/env', '@babel/react' ],
-				},
+				query : {
+					presets : [ '@babel/env' , '@babel/react' ] ,
+					plugins : [
+						[
+							"@babel/plugin-proposal-class-properties" ,
+							{
+								"loose" : true
+							}
+						]
+					]
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+				],
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader',
+				],
 			},
 		],
+	},
+	resolve: {
+		modules: [`${__dirname}/src`, 'node_modules'],
+		extensions: ['.js', '.jsx'],
 	},
     plugins: [
         new HtmlWebpackPlugin({
