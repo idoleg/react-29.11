f905304f0abf6041dbcf11a83bf0ae3ff669e312
petrGB <49945163+petrGB@users.noreply.github.com>
2019-12-10 14:29:54 +0300

    hw2 Petr

--- Petr/src/index.js
+++ Petr/src/index.js
@@ -1,17 +1,62 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MessageField from './components/MessageField';
 
-const messages = [
-    { id: 0, name: "Petr", content: "Привет!" },
-    { id: 1, name: "Ivan", content: "как дела?" },
-    { id: 3, name: "Petr", content: "Good" },
-    { id: 4, name: "", content: "Good" },
-]
-
-const Message = (props) => <div><b>{props.name}:</b> {props.content}</div>;
-const MessageList = function (props) {
-    return props.messages.map(
-        item => <Message name={item.name} content={item.content} key={item.id} />);
+function SendButton(props) {
+    return <button onClick={props.onClick}>{props.name}</button>;
 }
 
-ReactDOM.render(<MessageList messages={messages} />, document.getElementById("root"))
+class Container extends React.Component {
+    state = {
+        messages: [
+            { id: 0, name: "Oleg", content: "Привет!" },
+            { id: 1, name: "Ivan", content: "как дела?" },
+            { id: 3, name: "Oleg", content: "Good" },
+            { id: 4, name: "", content: "Good" },
+        ]
+    }
+
+    handleClick = (event) => {
+
+        event.preventDefault();
+
+        const name = document.getElementById('user').value;
+        const content = document.getElementById('content').value;
+
+        const count = this.state.messages.length;
+        this.setState({ messages: [...this.state.messages, { id: count + 1, name: name, content: content }] });
+    }
+
+    componentDidUpdate() {
+        const nameRobot = "Bot";
+        const count = this.state.messages.length;
+        const copyArray = this.state.messages;
+        const [lastMessage] = [...copyArray].reverse();
+        if (lastMessage.name != nameRobot) {
+            setTimeout(() => this.setState({ messages: [...this.state.messages, { id: count + 1, name: nameRobot, content: "Ok" }] }), 1000);
+        }
+    }
+
+    render() {
+
+        return (
+            <div>
+                <h1>MESSAGE SYSTEM</h1>
+                <MessageField messages={this.state.messages} />
+                <form actio>
+                    <input name="user" id="user" type="text" placeholder="Имя пользователя"></input><br />
+                    <textarea name="content" id="content" placeholder="Введите текст" required="required"></textarea><br />
+                    <SendButton
+                        name="Отправить"
+                        onClick={this.handleClick.bind(this)}
+                    />
+                </form>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Container />,
+    document.getElementById('root'),
+);
--- Petr/webpack.config.js
+++ Petr/webpack.config.js
@@ -14,7 +14,12 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: "babel-loader",
                 options: {
-                    presets: ["@babel/env", "@babel/react"]
+                    presets: ["@babel/env", "@babel/react"],
+                    plugins : [
+						[
+							"@babel/plugin-proposal-class-properties"
+						]
+					]
                 }
             }
         ]
