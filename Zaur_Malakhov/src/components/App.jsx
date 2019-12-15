7c30cd0752937b3d309f09462373361c2aef266d
zmalakhov <zm.13.15.zm@gmail.com>
2019-12-15 10:20:13 +0300

    add hw_04

--- Zaur_Malakhov/src/components/App.jsx
+++ Zaur_Malakhov/src/components/App.jsx
@@ -1,20 +1,24 @@
 import React, {Component} from "react";
-//import {Counter} from "./Counter";
 import {Messenger} from "./Messenger/Messenger";
+import {BrowserRouter, Switch, Route } from "react-router-dom";
+// import {Counter} from "./Counter";
+
+// www.site.ru/chats/1  www.site.ru/about - BrowserRouter
+// www.site.ru/#chats/1  www.site.ru/#about - HashRouter
+
 
 export class App extends Component {
-    state = {
-        isHidden: true,
-        counterValue: 0
-    }
-
-    handleCounter = (count) => {
-        this.setState({counterValue: count});
-    }
 
     render() {
         return (
-            <Messenger/>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={Messenger} />
+                    <Route exact path="/chats/:id" component={Messenger}/>
+                    {/*<Route exact path="/counter" render={() => <Counter init={4} onSaveCount={}}/>*/}
+                </Switch>
+            </BrowserRouter>
+
         );
     }
 }
